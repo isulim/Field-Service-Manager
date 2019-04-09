d03d6434a6ea4125fe90e352959dc55844203f56
isulim <ihor.sulym@gmail.com>
2019-04-09 20:27:37 +0200

    Favicon

--- FieldServiceManager/FieldServiceManager/urls.py
+++ FieldServiceManager/FieldServiceManager/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='ServiceJobs/login.html'),
          name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')), name='favicon'),
 ]
